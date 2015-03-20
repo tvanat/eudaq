92a46f7ffe7730e57ac72069e92f31f32a2e689e
Felix Bachmair <bachmair@phys.ethz.ch>
2015-03-20 08:36:02 +0100

    working on configuration, fixed raw event writing

--- producers/drs4/src/DRS4Producer.cxx
+++ producers/drs4/src/DRS4Producer.cxx
@@ -25,7 +25,7 @@ DRS4Producer::DRS4Producer(const std::string & name, const std::string & runcont
 		m_producerName(name),
 		m_event_type("DRS4"){
 	m_t = new eudaq::Timer;
-	n_channels = 8;
+	n_channels = 4;
 	cout<<"Started DRS4Producer with Name: "<<name<<endl;
 	m_b = 0;
 	m_drs = 0;
@@ -40,17 +40,17 @@ void DRS4Producer::OnStartRun(unsigned runnumber) {
 		std::cout<<"Create event"<<m_event_type<<" "<<m_run<<std::endl;
 		eudaq::RawDataEvent bore(eudaq::RawDataEvent::BORE(m_event_type, m_run));
 		std::cout << "drs4 board"<<std::endl;
-//		bore.SetTag("DRS4_BOARD", std::to_string(m_b->GetBoardSerialNumber()));
-//
-//		// Set Firmware name
-//		std::cout << "Set tag fw"<<std::endl;
-//		bore.SetTag("DRS4_FW", std::to_string(m_b->GetFirmwareVersion()));
-//		// Set Names for different channels
-//		for (int ch = 0; ch < n_channels; ch++){
-//			string tag = "CH_%d"+std::to_string(ch);
-//			std::cout << "Set tag"<<tag<<std::endl;
-//			bore.SetTag(tag, "BLA");
-//		}
+		//		bore.SetTag("DRS4_BOARD", std::to_string(m_b->GetBoardSerialNumber()));
+		//
+		//		// Set Firmware name
+		//		std::cout << "Set tag fw"<<std::endl;
+		//		bore.SetTag("DRS4_FW", std::to_string(m_b->GetFirmwareVersion()));
+		//		// Set Names for different channels
+		//		for (int ch = 0; ch < n_channels; ch++){
+		//			string tag = "CH_%d"+std::to_string(ch);
+		//			std::cout << "Set tag"<<tag<<std::endl;
+		//			bore.SetTag(tag, "BLA");
+		//		}
 		std::cout << "Send event"<<std::endl;
 		// Send the event out:
 		SendEvent(bore);
@@ -117,20 +117,16 @@ void DRS4Producer::ReadoutLoop() {
 				/* read all waveforms */
 				m_b->TransferWaves(0, 8);
 
-				/* read time (X) array of first channel in ns */
-				m_b->GetTime(0, 0, m_b->GetTriggerCell(0), time_array[0]);
 
-				/* decode waveform (Y) array of first channel in mV */
-				m_b->GetWave(0, 0, wave_array[0]);
-
-				/* read time (X) array of second channel in ns
-				   Note: On the evaluation board input #1 is connected to channel 0 and 1 of
-				   the DRS chip, input #2 is connected to channel 2 and 3 and so on. So to
-				   get the input #2 we have to read DRS channel #2, not #1. */
-				m_b->GetTime(0, 2, m_b->GetTriggerCell(0), time_array[1]);
-
-				/* decode waveform (Y) array of second channel in mV */
-				m_b->GetWave(0, 2, wave_array[1]);
+				for (int ch = 0; ch < n_channels; ch++){
+					/* read time (X) array of  channel in ns
+					   Note: On the evaluation board input #1 is connected to channel 0 and 1 of
+					   the DRS chip, input #2 is connected to channel 2 and 3 and so on. So to
+					   get the input #2 we have to read DRS channel #2, not #1. */
+					m_b->GetTime(0, ch*2, m_b->GetTriggerCell(0), time_array[ch]);
+					/* decode waveform (Y) array of first channel in mV */
+					m_b->GetWave(0, ch*2, wave_array[ch]);
+				}
 
 				/* Restart Readout */
 				m_b->StartDomino();
@@ -142,8 +138,13 @@ void DRS4Producer::ReadoutLoop() {
 				//
 				//				  float time_array[8][1024];
 				//				  float wave_array[8][1024];
-				ev.AddBlock(0, reinterpret_cast<const char*>(&time_array[0]), sizeof( time_array[0][0])*8*1024);
-				ev.AddBlock(0, reinterpret_cast<const char*>(&wave_array[0]), sizeof( wave_array[0][0])*8*1024);
+
+				for (int ch = 0; ch < n_channels; ch++){
+					/* Set time block of ch */
+					ev.AddBlock(ch*2+0, reinterpret_cast<const char*>(&time_array[ch]), sizeof( time_array[ch][0])*1024);
+					/* Set data block of ch */
+					ev.AddBlock(ch*2+1, reinterpret_cast<const char*>(&wave_array[ch]), sizeof( wave_array[ch][0])*1024);
+				}
 
 				SendEvent(ev);
 				m_ev++;
@@ -164,53 +165,70 @@ void DRS4Producer::ReadoutLoop() {
 };
 
 void DRS4Producer::OnConfigure(const eudaq::Configuration& conf) {
-
+	cout << "Configure DRS4 board"<<endl;
 	m_config = conf;
-	/* do initial scan */
-	m_drs = new DRS();
 
-	/* show any found board(s) */
-	for (size_t i=0 ; i<m_drs->GetNumberOfBoards() ; i++) {
-		m_b = m_drs->GetBoard(i);
-		printf("Found DRS4 evaluation board, serial #%d, firmware revision %d\n",
-				m_b->GetBoardSerialNumber(), m_b->GetFirmwareVersion());
+	try {
+		/* do initial scan */
+		cout<<"Get DRS() ..."<<endl;
+		if (m_drs)
+			delete m_drs;
+		m_drs = new DRS();
+		cout<<"Show boards..."<<endl;
+		int nBoards = m_drs->GetNumberOfBoards();
+		cout<<"There are "<<nBoards<<" DRS4-Evaluation-Board(s) connected"<<endl;
+		/* show any found board(s) */
+		for (size_t i=0 ; i<m_drs->GetNumberOfBoards() ; i++) {
+			m_b = m_drs->GetBoard(i);
+			printf("Found DRS4 evaluation board, serial #%d, firmware revision %d\n",
+					m_b->GetBoardSerialNumber(), m_b->GetFirmwareVersion());
+		}
+
+		/* exit if no board found */
+		if (!nBoards){
+
+			EUDAQ_ERROR(string("No Board connected exception."));
+			SetStatus(eudaq::Status::LVL_ERROR, "No Board connected.");
+			return;
+		}
+
+
+		/* continue working with first board only */
+		m_b = m_drs->GetBoard(0);
+
+
+		/* initialize board */
+		m_b->Init();
+
+		/* set sampling frequency */
+		m_b->SetFrequency(5, true);
+
+		/* enable transparent mode needed for analog trigger */
+		m_b->SetTranspMode(1);
+
+		/* set input range to -0.5V ... +0.5V */
+		m_b->SetInputRange(0);
+
+		/* use following line to set range to 0..1V */
+		//b->SetInputRange(0.5);
+
+		/* use following line to turn on the internal 100 MHz clock connected to all channels  */
+		m_b->EnableTcal(1);
+
+		/* use following lines to enable hardware trigger on CH1 at 50 mV positive edge */
+		if (m_b->GetBoardType() >= 8) {        // Evaluation Board V4&5
+			m_b->EnableTrigger(1, 0);           // enable hardware trigger
+			m_b->SetTriggerSource(1<<0);        // set CH1 as source
+		} else if (m_b->GetBoardType() == 7) { // Evaluation Board V3
+			m_b->EnableTrigger(0, 1);           // lemo off, analog trigger on
+			m_b->SetTriggerSource(0);           // use CH1 as source
+		}
+		m_b->SetTriggerLevel(-0.05);            // 0.05 V
+		m_b->SetTriggerPolarity(false);        // positive edge
+	}catch ( ... ){
+		EUDAQ_ERROR(string("Unknown exception."));
+		SetStatus(eudaq::Status::LVL_ERROR, "Unknown exception.");
 	}
-
-	/* exit if no board found */
-	int nBoards = m_drs->GetNumberOfBoards();
-
-	/* continue working with first board only */
-	m_b = m_drs->GetBoard(0);
-
-
-	/* initialize board */
-	m_b->Init();
-
-	/* set sampling frequency */
-	m_b->SetFrequency(5, true);
-
-	/* enable transparent mode needed for analog trigger */
-	m_b->SetTranspMode(1);
-
-	/* set input range to -0.5V ... +0.5V */
-	m_b->SetInputRange(0);
-
-	/* use following line to set range to 0..1V */
-	//b->SetInputRange(0.5);
-
-	/* use following line to turn on the internal 100 MHz clock connected to all channels  */
-	m_b->EnableTcal(1);
-
-	/* use following lines to enable hardware trigger on CH1 at 50 mV positive edge */
-	if (m_b->GetBoardType() >= 8) {        // Evaluation Board V4&5
-		m_b->EnableTrigger(1, 0);           // enable hardware trigger
-		m_b->SetTriggerSource(1<<0);        // set CH1 as source
-	} else if (m_b->GetBoardType() == 7) { // Evaluation Board V3
-		m_b->EnableTrigger(0, 1);           // lemo off, analog trigger on
-		m_b->SetTriggerSource(0);           // use CH1 as source
-	}
-	m_b->SetTriggerLevel(-0.05);            // 0.05 V
-	m_b->SetTriggerPolarity(false);        // positive edge
 }
 
 
